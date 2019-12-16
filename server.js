917be6ef780c928d899e1bcaadc38c780b1305db
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-12-16 07:52:19 -0500

    connected to db

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import { json, urlencoded } from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
+import usersRouter from "./routes/api/users";
+import postsRouter from "./routes/api/posts";
+import profileRouter from "./routes/api/profile";
 
 const app = express();
 
 import "dotenv/config";
+
 mongoose
-  .connect(`${process.env.MONGODB_URI}`, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -22,10 +26,10 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// app.get("/", (req, res) => res.send("Hello World"));
-// app.use("/users", require("./routes/api/users"));
-// app.use("/api/post", require("./routes/api/post"));
-// app.use("/api/profile", require("./routes/api/profile"));
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", usersRouter);
+app.use("/api/post", postsRouter);
+app.use("/api/profile", profileRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
