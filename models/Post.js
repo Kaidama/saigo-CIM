edb21c0477a5b9dabe9cea68e26aab298f5fbd6c
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-12-17 21:21:27 -0500

    created user schema

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.schema({
+ user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export const Post = mongoose.model('post', postSchema)
